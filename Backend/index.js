906f4794f347bce4ebd27c05e5bbbef113c26e79
Pedro4654 <laerciobrito4535@gmail.com>
2026-03-03 18:56:14 -0300

    Projeto Crud

--- Backend/index.js
+++ Backend/index.js
@@ -70,7 +70,7 @@ app.delete('/produtos/:id', (req, res) => {
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
 // Redireciona qualquer rota que não seja da API para o Vue.js
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
 
